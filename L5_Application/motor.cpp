fd5fbdb80b3411ba79e89ea9fd60521421d26736
Cynthia Ho <cynthiahosy@gmail.com>
2015-10-22 19:10:07 -0700

    add throttle/servo control and speed monitor

---
+++ L5_Application/motor.cpp
@@ -0,0 +1,211 @@
+#include "io.hpp"
+#include "eint.h"
+#include "gpio.hpp"
+#include "lpc_pwm.hpp"
+#include "motor.hpp"
+
+#include <cstdio>
+
+DirectionCtrl * DirectionCtrl::m_pInstance = NULL;
+DirectionCtrl::DirectionCtrl(void) :
+        directionPWM(PWM(PWM::pwm1, STANDARD_FREQ)),
+        pin0_0(P0_0), pin0_1(P0_1)
+{
+    dirPWM = basePWM;
+    directionPWM.set(dirPWM);
+    pin0_0.setAsOutput();
+    pin0_1.setAsOutput();
+}
+DirectionCtrl * DirectionCtrl::getInstance()
+{
+    if (m_pInstance == NULL)
+        m_pInstance = new DirectionCtrl();
+    return m_pInstance;
+}
+void DirectionCtrl::setDirection(int dir)
+{
+    switch (dir){
+        case dirFarRight:
+            dirPWM = farRight;
+            pin0_0.setHigh();
+            pin0_1.setLow();
+            break;
+        case dirRight:
+            dirPWM = right;
+            pin0_0.setHigh();
+            pin0_1.setLow();
+            break;
+        case dirCenter:
+            dirPWM = basePWM;
+            pin0_0.setLow();
+            pin0_1.setLow();
+            break;
+        case dirLeft:
+            dirPWM = left;
+            pin0_1.setHigh();
+            pin0_0.setLow();
+            break;
+        case dirFarLeft:
+            dirPWM = farLeft;
+            pin0_1.setHigh();
+            pin0_0.setLow();
+            break;
+
+    }
+    printf("PWM %f\n", dirPWM);
+    directionPWM.set(dirPWM);
+
+};
+
+SpeedCtrl* SpeedCtrl::m_pInstance = NULL;
+SpeedCtrl::SpeedCtrl():
+        throttlePWM(PWM(PWM::pwm2, STANDARD_FREQ)),
+        pin1_20(P1_20), pin1_23(P1_23)
+{
+    speedPWM = basePWM;
+    throttlePWM.set(basePWM);
+    LD.setNumber(speedPWM);
+    pin1_20.setAsOutput();
+    pin1_23.setAsOutput();
+}
+
+SpeedCtrl* SpeedCtrl::getInstance(){
+    if (m_pInstance == NULL)
+        m_pInstance = new SpeedCtrl();
+    return m_pInstance;
+}
+
+void SpeedCtrl::initESC()
+{
+
+    printf("INIT\n");
+    throttlePWM.set(SpeedCtrl::frontLimitPWM);
+    vTaskDelay(1000);
+    throttlePWM.set(SpeedCtrl::backLimitPWM);
+    vTaskDelay(1000);
+    printf("INIT Done\n");
+    throttlePWM.set(SpeedCtrl::basePWM);
+    speedPWM = SpeedCtrl::basePWM;
+    LD.setNumber((int)speedPWM);
+
+}
+bool SpeedCtrl::checkPWM(float pwm)
+{
+    printf("Check speed %f\n", pwm);
+    //if ((pwm > backLimitPWM) && (pwm < frontLimitPWM))
+    //{
+        if ( basePWM - pwm > 0.5)
+        {
+            pin1_20.setHigh();
+            pin1_23.setHigh();
+        }
+        else{
+            pin1_20.setLow();
+            pin1_23.setLow();
+        }
+        return true;
+    //}
+    //return false;
+}
+
+void SpeedCtrl::setSpeedPWM(float pwm)
+{
+    if (checkPWM(pwm)){
+        printf("Set speed %f\n", pwm);
+        if ((( speedPWM < SpeedCtrl::basePWM ) && (pwm > SpeedCtrl::basePWM)) ||
+                (( speedPWM > SpeedCtrl::basePWM ) && (pwm < SpeedCtrl::basePWM)))
+        { //Change forward to backward or backward to forward
+            throttlePWM.set(SpeedCtrl::basePWM-0.3);
+            vTaskDelay(1000);
+            throttlePWM.set(SpeedCtrl::basePWM);
+            vTaskDelay(1000);
+        }
+        throttlePWM.set(pwm);
+        speedPWM = pwm;
+    }
+}
+
+void SpeedCtrl::incrSpeedPWM()
+{
+    float pwm = speedPWM + PWMStep;
+    if (checkPWM(pwm)){
+        printf("Set speed %f\n", pwm);
+        speedPWM = pwm;
+        throttlePWM.set(pwm);
+    }
+    else
+    {
+        printf("This(PWM:%f) is out of limit\n", pwm);
+    }
+}
+
+void SpeedCtrl::descrSpeedPWM()
+{
+    float pwm = speedPWM - PWMStep;
+    if (checkPWM(pwm)){
+        printf("Set speed %f\n", pwm);
+        speedPWM = pwm;
+        throttlePWM.set(pwm);
+    }
+    else
+    {
+        printf("This(PWM:%f) is out of limit\n", pwm);
+    }
+}
+SpeedMonitor* SpeedMonitor::m_pInstance = NULL;
+void speed_pulse_start(void)
+{
+    SpeedMonitor::getInstance()->calSpeed();
+}
+
+SpeedMonitor::SpeedMonitor()
+{
+    m_rpm = 0;
+    m_speed = 0;
+    //Singleton
+    int port2_5 = 5;
+    eint3_enable_port2(port2_5, eint_rising_edge, speed_pulse_start);
+}
+
+SpeedMonitor* SpeedMonitor::getInstance(){
+    if (m_pInstance == NULL)
+        m_pInstance = new SpeedMonitor();
+    return m_pInstance;
+}
+
+void SpeedMonitor::setRpm(int rpmVal)
+{
+    m_rpm = rpmVal;
+    m_speed = 0.1 *3.1415 *((float)m_rpm* 60); //meter
+}
+void SpeedMonitor::getSpeed(float* rpm, float* speed)
+{
+    const uint64_t FIVE_SECOND = 5* 1000;
+    uint64_t cur_time = sys_get_uptime_ms();
+    if (cur_time - m_last_time > FIVE_SECOND)
+    {
+        m_speed = 0;
+        m_rpm = 0;
+    }
+    *rpm = m_rpm;
+    *speed = m_speed_meter;
+}
+int SpeedMonitor::getRpm()
+{
+    return m_rpm;
+}
+float SpeedMonitor::getSpeedMeter()
+{
+    return m_speed_meter;
+}
+
+void SpeedMonitor::calSpeed(){
+    uint64_t cur_time = sys_get_uptime_ms();
+    uint64_t time_diff = cur_time - m_last_time;
+
+    m_rpm = 1.0/ ( (float)time_diff * MS_TO_MINS);
+    m_speed_meter = DIAMETER *PI * (m_rpm* 60); // m/hr
+    m_speed = m_speed_meter /1000 * KM_TO_MILES;
+    m_last_time = cur_time;
+}
+
---
+++ L5_Application/motor.hpp
@@ -0,0 +1,82 @@
+#include "gpio.hpp"
+#include "lpc_pwm.hpp"
+
+#define STANDARD_FREQ 55
+#define PI 3.1415
+#define DIAMETER 0.1 //meter
+#define KM_TO_MILES 1/1.60934
+#define MS_TO_MINS 1/(1000 * 60)
+#define MAX_DUTY_CYCLE 11
+#define MIN_DUTY_CYCLE 5.5
+#define BASE_DUTY_CYCLE ((MAX_DUTY_CYCLE+MIN_DUTY_CYCLE)/2)
+
+class DirectionCtrl
+{
+    public:
+        enum
+        {
+            dirFarRight = 2,
+            dirRight = 1,
+            dirCenter = 0,
+            dirLeft = -1,
+            dirFarLeft = -2
+        };
+        static DirectionCtrl * getInstance();
+        void setDirection(int dir);
+
+    private:
+        DirectionCtrl();
+        static DirectionCtrl * m_pInstance;
+        PWM directionPWM;
+        GPIO pin0_0;
+        GPIO pin0_1;
+        float dirPWM = 0;
+        const float halfTurn = ((MAX_DUTY_CYCLE - BASE_DUTY_CYCLE) / 2);
+        const float basePWM = BASE_DUTY_CYCLE;
+        const float farRight = MAX_DUTY_CYCLE;
+        const float farLeft = MIN_DUTY_CYCLE;
+        const float right = basePWM + halfTurn;
+        const float left = basePWM - halfTurn;
+};
+
+class SpeedCtrl
+{
+    public:
+        static SpeedCtrl * getInstance();
+        void initESC();
+        void setSpeed(float speed);bool checkPWM(float pwm);
+        void setSpeedPWM(float pwm);
+        void incrSpeedPWM();
+        void descrSpeedPWM();
+
+    private:
+        SpeedCtrl();
+        static SpeedCtrl * m_pInstance;
+        PWM throttlePWM;
+        GPIO pin1_20;
+        GPIO pin1_23;
+        float speedPWM = 0;
+        const float PWMStep = 0.1;
+        const float basePWM = BASE_DUTY_CYCLE;
+        const float frontLimitPWM = MAX_DUTY_CYCLE;
+        const float backLimitPWM = MIN_DUTY_CYCLE;
+};
+
+class SpeedMonitor
+{
+    public:
+        static SpeedMonitor * getInstance();
+        void setRpm(int rpmVal);
+        void calSpeed();
+        int getRpm();
+        float getSpeedMeter();
+        void getSpeed(float* rpm, float* speed);
+
+    private:
+        SpeedMonitor();
+        static SpeedMonitor * m_pInstance;
+        float m_speed;
+        float m_rpm;
+        float m_speed_meter = 0;
+        uint64_t m_last_time = 0;
+};

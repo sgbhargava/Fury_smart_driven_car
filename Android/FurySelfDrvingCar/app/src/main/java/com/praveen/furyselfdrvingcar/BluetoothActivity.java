02f9c3d0f73925d2105b9515e70502e1565d59af
Praveen Prabhakaran <praveen.prabhakaran1903@gmail.com>
2015-10-26 01:56:30 -0700

    Andriod Main App Destination location point updated

--- Android/FurySelfDrvingCar/app/src/main/java/com/praveen/furyselfdrvingcar/BluetoothActivity.java
+++ Android/FurySelfDrvingCar/app/src/main/java/com/praveen/furyselfdrvingcar/BluetoothActivity.java
@@ -4,4 +4,5 @@ package com.praveen.furyselfdrvingcar;
  * Created by prave on 10/25/2015.
  */
 public class BluetoothActivity {
+
 }
--- Android/FurySelfDrvingCar/app/src/main/java/com/praveen/furyselfdrvingcar/MapsActivity.java
+++ Android/FurySelfDrvingCar/app/src/main/java/com/praveen/furyselfdrvingcar/MapsActivity.java
@@ -29,6 +29,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         addListenerOnButton();
+         final GoogleMap googleMap = mapFragment.getMap();
+
+        // Setting a click event handler for the map
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                //googleMap.clear();
+
+                // Animating to the touched position
+                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                googleMap.addMarker(markerOptions);
+            }
+        });
 
     }
 
@@ -60,6 +88,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
         });
+        
     }
 
 
@@ -78,15 +107,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
 
-        LatLng initialLocation = new LatLng(37.3334, -121.80856);
+        LatLng initialLocation = new LatLng(37.33538164, -121.88124232);
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(initialLocation)      // Sets the center of the map to LatLng (refer to previous snippet)
-                .zoom(17)                   // Sets the zoom
+                .zoom(20)                   // Sets the zoom
                 .bearing(90)                // Sets the orientation of the camera to east
                 .tilt(30)                   // Sets the tilt of the camera to 30 degrees
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        mMap.addMarker(new MarkerOptions().position(initialLocation).title("Initial Car Location").snippet("37.3334, -121.80856"));
+        mMap.addMarker(new MarkerOptions().position(initialLocation).title("Initial Car Location").snippet("37.33538164, -121.88124232"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(initialLocation));
     }
 }
